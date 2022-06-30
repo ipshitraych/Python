7eed1e26e9be8c08e205c1a8a48bae0db03ae710
Ipshit Raychaudhuri <107639636+ipshitraych@users.noreply.github.com>
2022-06-30 22:36:26 +0100

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,48 +1,67 @@
 import random
 
+res = 0
 pscore = 0
 bscore = 0
 play = 0
 game = True
+prompt = True
 
 pchoice = {"ROCK": 1,"PAPER": 2,"SCISSORS": 3}
 bchoice = {1: "ROCK", 2: "PAPER", 3: "SCISSORS"}
 
 print("Rock, paper, scissors")
-rounds = int(input("Choose the number of rounds: "))
 
-while game:
+while prompt:
 
-    pinput = input("\nThrow: ")
-    pla = pchoice[pinput.upper()]
+    rounds = int(input("Choose the number of rounds: "))
 
-    bot = random.choice(range(1, 3))
-    print("I choose" ,bchoice[bot])
+    while game:
 
-    con = bot - pla
-    if con == -1 or con == 2:
-        print("Round won")
-        pscore += 1
-    elif con == 1 or con == -2:
-        print("Round lost")
-        bscore +=1
-    else:
-        print("Round drawn")
+        pinput = input("\nThrow: ")
+        pla = pchoice[pinput.upper()]
 
-    play += 1
-    
-    if rounds/2 < bscore :
-        game = False
-        print("\nBOT wins the game")
-    elif rounds/2 < pscore:
-        game = False
+        bot = random.choice(range(1, 3))
+        print("I choose" ,bchoice[bot])
+
+        con = bot - pla
+        if con == -1 or con == 2:
+            print("Round won")
+            pscore += 1
+        elif con == 1 or con == -2:
+            print("Round lost")
+            bscore +=1
+        else:
+            print("Round drawn")
+
+        play += 1
+        
+        if rounds/2 < bscore :
+            game = False
+            res = -1
+        elif rounds/2 < pscore:
+            game = False
+            res = 1
+
+        if rounds == play:
+            game = False
+            if pscore > bscore:
+                res = 1
+            elif bscore > pscore:
+                res = -1    
+            elif pscore - bscore == 0:
+                res = 0
+                
+    if res == 1:
         print("\nPLAYER wins the game")
+    elif res == 0:
+        print("\nThe game ends in a draw")
+    else:
+        print("\nCOMP wins the game")
 
-    if rounds == play:
-        game = False
-        if pscore > bscore:
-            print("\nPLAYER wins the game")
-        elif bscore > pscore:
-            print("\nBOT wins the game")
-        elif pscore - bscore == 0:
-            print("\nThe game ends in a draw")
+    choice = input("\nAnother game? (Y/N) ")
+    if choice == "N":
+        prompt = False
+    else:
+        game = True
+        play = 0 
